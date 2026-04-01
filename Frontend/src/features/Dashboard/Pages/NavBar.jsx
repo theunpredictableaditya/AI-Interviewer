59b2c00914953542f282eaadf7bcc345b1b108dc
Aditya Kumar Gupta <adityakgupta206@gmail.com>
2026-04-01 20:31:42 +0545

    feat: built ui of navbar at dashboard

--- Frontend/src/features/Dashboard/Pages/NavBar.jsx
+++ Frontend/src/features/Dashboard/Pages/NavBar.jsx
@@ -1,9 +1,28 @@
 import React from 'react'
+import search from "../../../assets/search.svg"
+import setting from "../../../assets/homeSetting.svg"
+import notification from "../../../assets/notification.svg"
+import "../Styles/NavBar.scss"
 
 const NavBar = () => {
   return (
     <div className='navbar'>
-      NavBar
+      <div className="navRight">
+        <div className="white">Intelligence</div>
+        <div className="blue">Hub</div>
+      </div>
+      <div className="navLeft">
+        <div className="search">
+          <img src={search} alt="search" />
+          <input type="text" placeholder='Search Insight...'/>
+        </div>
+        <div className="notification">
+          <img src={notification} alt="notification" />
+        </div>
+        <div className="settings">
+          <img src={setting} alt="setting" />
+        </div>
+      </div>
     </div>
   )
 }
--- Frontend/src/features/Dashboard/Pages/SideBar.jsx
+++ Frontend/src/features/Dashboard/Pages/SideBar.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import "../Styles/SideBar.scss"
 
+
 const SideBar = () => {
   return (
     <div className='sidebar'>
